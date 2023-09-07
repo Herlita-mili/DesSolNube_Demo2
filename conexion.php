adfd62b06f98ed0ad380cf602e57c4147d1778f1
Herlita <herlita.farro@tecsup.edu.pe>
2023-09-07 12:10:35 -0500

    sesion2_v1

--- conexion.php
+++ conexion.php
@@ -2,11 +2,11 @@
 
 	function conexion(){
 
-	$host = "containers-us-west-162.railway.app";
-	$port = "port=7001";
+	$host = "containers-us-west-81.railway.app";
+	$port = "port=7484";
 	$dbname = "dbname=railway";
-	$user = "user=postgres";
-	$password = "3aSGidfJsw5TDkUL2Lp3";
+	$user = "user=root";
+	$password = "VzJe12oKTjjNY3K99Ib7";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
